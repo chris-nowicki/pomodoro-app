f59617f429608545084250e99aae481b069772f3
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-12-09 09:08:57 -0500

    updated button to a link

--- src/App.jsx
+++ src/App.jsx
@@ -115,118 +115,116 @@ function App() {
 	}
 
 	return (
-		<>
-			<div className="flex flex-row justify-center">
-				{/* pomodoro app container */}
-				<div className="flex flex-col items-center">
-					<span className="mt-12">pomodoro</span>
-					{/* mode menu container */}
-					<nav className="modeMenu z-20 flex flex-row items-center justify-evenly">
-						<ModeButton
-							name="pomodoro"
-							data="pomodoro"
-							color={settings.color}
-							active={currentMode}
-							onClick={() => updateMode("pomodoro")}
-						/>
-						<ModeButton
-							name="short break"
-							data="shortBreak"
-							color={settings.color}
-							active={currentMode}
-							onClick={() => updateMode("shortBreak")}
-						/>
-						<ModeButton
-							name="long break"
-							data="longBreak"
-							color={settings.color}
-							active={currentMode}
-							onClick={() => updateMode("longBreak")}
-						/>
-					</nav>
+		<div className="flex flex-row justify-center">
+			{/* pomodoro app container */}
+			<div className="flex flex-col items-center">
+				<span className="mt-12">pomodoro</span>
+				{/* mode menu container */}
+				<nav className="modeMenu z-20 flex flex-row items-center justify-evenly">
+					<ModeButton
+						name="pomodoro"
+						data="pomodoro"
+						color={settings.color}
+						active={currentMode}
+						onClick={() => updateMode("pomodoro")}
+					/>
+					<ModeButton
+						name="short break"
+						data="shortBreak"
+						color={settings.color}
+						active={currentMode}
+						onClick={() => updateMode("shortBreak")}
+					/>
+					<ModeButton
+						name="long break"
+						data="longBreak"
+						color={settings.color}
+						active={currentMode}
+						onClick={() => updateMode("longBreak")}
+					/>
+				</nav>
 
-					{/* timer container */}
-					<main className="timer-container flex flex-col items-center justify-center">
-						{/* main circle */}
-						<div className="circle relative flex items-center justify-center">
-							{/* progress bar */}
+				{/* timer container */}
+				<main className="timer-container flex flex-col items-center justify-center">
+					{/* main circle */}
+					<div className="circle relative flex items-center justify-center">
+						{/* progress bar */}
 
-							<svg>
-								{!matches ? (
-									<circle cx="174" cy="174" r="163" />
-								) : (
-									<circle cx="124" cy="124" r="114" />
-								)}
-							</svg>
-							{/* end of progress circle */}
+						<svg>
+							{!matches ? (
+								<circle cx="174" cy="174" r="163" />
+							) : (
+								<circle cx="124" cy="124" r="114" />
+							)}
+						</svg>
+						{/* end of progress circle */}
 
-							{/* clock container */}
-							<div className="clock-display z-10 flex w-full flex-col items-center">
-								<div className="flex h-[132px] w-full items-center justify-center sm:ml-[38px] md:ml-[48px]">
-									<h1
-										id="js-minutes"
-										className="flex w-[132px] justify-end"
-									>
-										{`${remainingTime.minutes}`.padStart(
-											2,
-											"0"
-										)}
-									</h1>
-									<h1 id="js-colon" className="w-auto">
-										:
-									</h1>
-									<h1 id="js-seconds" className="w-[180px]">
-										{`${remainingTime.seconds}`.padStart(
-											2,
-											"0"
-										)}
-									</h1>
-								</div>
-								<div className="ml-4 flex w-full flex-row justify-center md:mt-[24px]">
-									<button
-										id="action-button"
-										onClick={() => handleAction()}
-									>
-										{action}
-									</button>
-								</div>
+						{/* clock container */}
+						<div className="clock-display z-10 flex w-full flex-col items-center">
+							<div className="flex h-[132px] w-full items-center justify-center sm:ml-[38px] md:ml-[48px]">
+								<h1
+									id="js-minutes"
+									className="flex w-[132px] justify-end"
+								>
+									{`${remainingTime.minutes}`.padStart(
+										2,
+										"0"
+									)}
+								</h1>
+								<h1 id="js-colon" className="w-auto">
+									:
+								</h1>
+								<h1 id="js-seconds" className="w-[180px]">
+									{`${remainingTime.seconds}`.padStart(
+										2,
+										"0"
+									)}
+								</h1>
+							</div>
+							<div className="ml-4 flex w-full flex-row justify-center md:mt-[24px]">
+								<button
+									id="action-button"
+									onClick={() => handleAction()}
+								>
+									{action}
+								</button>
 							</div>
 						</div>
-					</main>
-					{/* settings icon */}
-					<a
-						onClick={() => setShowSettings(true)}
-						className="settings-button"
+					</div>
+				</main>
+				{/* settings icon */}
+				<a
+					onClick={() => setShowSettings(true)}
+					className="settings-button"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="28"
+						height="28"
+						className="sm:mt-[79px] md:mt-[63px]"
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="28"
-							height="28"
-							className="sm:mt-[79px] md:mt-[63px]"
-						>
-							<path
-								fill="#D7E0FF"
-								d="M26.965 17.682l-2.927-2.317c.055-.448.097-.903.097-1.365 0-.462-.042-.917-.097-1.365l2.934-2.317a.702.702 0 00.167-.896l-2.775-4.851a.683.683 0 00-.847-.301l-3.454 1.407a10.506 10.506 0 00-2.345-1.379l-.52-3.71A.716.716 0 0016.503 0h-5.55a.703.703 0 00-.687.588l-.52 3.71c-.847.357-1.63.819-2.345 1.379L3.947 4.27a.691.691 0 00-.847.301L.325 9.422a.705.705 0 00.167.896l2.927 2.317c-.055.448-.097.903-.097 1.365 0 .462.042.917.097 1.365L.492 17.682a.702.702 0 00-.167.896L3.1 23.429a.683.683 0 00.847.301L7.4 22.323a10.506 10.506 0 002.345 1.379l.52 3.71c.056.329.34.588.687.588h5.55a.703.703 0 00.687-.588l.52-3.71c.847-.357 1.631-.819 2.346-1.379l3.454 1.407c.313.119.673 0 .847-.301l2.775-4.851a.705.705 0 00-.167-.896zM13.73 18.9c-2.685 0-4.857-2.191-4.857-4.9 0-2.709 2.172-4.9 4.857-4.9 2.684 0 4.856 2.191 4.856 4.9 0 2.71-2.172 4.9-4.856 4.9z"
-								opacity=".5"
-								className="settings-icon"
-							/>
-						</svg>
-					</a>
-				</div>
-				{showSettings && (
-					<SettingsMenu
-						settings={settings}
-						setSettings={setSettings}
-						setShowSettings={setShowSettings}
-						currentMode={currentMode}
-						countdown={countdown}
-						updateRemainingTime={(mode) =>
-							setRemainingTime(calculateTime(mode))
-						}
-					/>
-				)}
+						<path
+							fill="#D7E0FF"
+							d="M26.965 17.682l-2.927-2.317c.055-.448.097-.903.097-1.365 0-.462-.042-.917-.097-1.365l2.934-2.317a.702.702 0 00.167-.896l-2.775-4.851a.683.683 0 00-.847-.301l-3.454 1.407a10.506 10.506 0 00-2.345-1.379l-.52-3.71A.716.716 0 0016.503 0h-5.55a.703.703 0 00-.687.588l-.52 3.71c-.847.357-1.63.819-2.345 1.379L3.947 4.27a.691.691 0 00-.847.301L.325 9.422a.705.705 0 00.167.896l2.927 2.317c-.055.448-.097.903-.097 1.365 0 .462.042.917.097 1.365L.492 17.682a.702.702 0 00-.167.896L3.1 23.429a.683.683 0 00.847.301L7.4 22.323a10.506 10.506 0 002.345 1.379l.52 3.71c.056.329.34.588.687.588h5.55a.703.703 0 00.687-.588l.52-3.71c.847-.357 1.631-.819 2.346-1.379l3.454 1.407c.313.119.673 0 .847-.301l2.775-4.851a.705.705 0 00-.167-.896zM13.73 18.9c-2.685 0-4.857-2.191-4.857-4.9 0-2.709 2.172-4.9 4.857-4.9 2.684 0 4.856 2.191 4.856 4.9 0 2.71-2.172 4.9-4.856 4.9z"
+							opacity=".5"
+							className="settings-icon"
+						/>
+					</svg>
+				</a>
 			</div>
-		</>
+			{showSettings && (
+				<SettingsMenu
+					settings={settings}
+					setSettings={setSettings}
+					setShowSettings={setShowSettings}
+					currentMode={currentMode}
+					countdown={countdown}
+					updateRemainingTime={(mode) =>
+						setRemainingTime(calculateTime(mode))
+					}
+				/>
+			)}
+		</div>
 	)
 }
 
